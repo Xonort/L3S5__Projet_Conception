637284c7da247c08f42e6f63a3ff67f7b5e4a3fa
pointel <arthurpointel@outlook.com>
2018-11-29 23:52:33 +0100

    Ajout de plein de fichier en copiant sur Chameaux

---
+++ src/VueIHMFX.java
@@ -0,0 +1,24 @@
+package sample;
+
+import javafx.scene.control.Button;
+
+
+public class VueIHMFX {
+
+    Button myButton = new Button("Choisir fichier sokoban");
+    //GridPane gridPane = new GridPane();
+
+    public VueIHMFX() {
+
+        myButton.setMinSize(80,80);
+
+        }
+        //dessine();
+
+
+    public void dessine() {
+
+        }
+
+}
+
